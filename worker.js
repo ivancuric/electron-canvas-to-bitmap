388c62e7668e9b641b78606f1cfc01febcf29a09
Ivan Čurić <ivan.curic@gmail.com>
2021-07-01 13:10:36 +0200

    Working worker (hah)

--- worker.js
+++ worker.js
@@ -4,11 +4,12 @@ const ctx2d = canvas2d.getContext("2d");
 const canvasBitmap = new OffscreenCanvas(3840, 2160);
 const ctxBitmap = canvasBitmap.getContext("bitmaprenderer");
 
-onmessage = (data) => {
-  //WHAT THE HELL
-  requestAnimationFrame(() => {
-    console.log(data.bitmap);
-  });
-  // ctx.drawImage(bitmap, 0, 0, canvas.width, canvas.height);
-  // const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+onmessage = ({ data }) => {
+  ctx2d.drawImage(data.bitmap, 0, 0, canvas2d.width, canvas2d.height);
+  const pixelData = ctx2d.getImageData(
+    0,
+    0,
+    canvas2d.width,
+    canvas2d.height
+  ).data;
 };
