4570c5cbc0c3801aa59db73a310423f87cc4809e
Ivan Čurić <ivan.curic@gmail.com>
2021-07-01 14:22:01 +0200

    Log dropped frames

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
--- renderer.js
+++ renderer.js
@@ -10,21 +10,36 @@
 
 const video = document.getElementById("potato");
 
+let latestFrame = 0;
+
 video.addEventListener("loadedmetadata", () => {
-  let canvas = new OffscreenCanvas(3840, 2160);
-  let ctx = canvas.getContext("2d");
-  // let frame = 0;
+  let canvas;
+  let ctx;
+
+  let canvas1 = new OffscreenCanvas(3840, 2160);
+  let canvas2 = new OffscreenCanvas(3840, 2160);
+  let ctx1 = canvas1.getContext("2d");
+  let ctx2 = canvas2.getContext("2d");
 
   const capture = async (time, meta) => {
-    // console.log(time, meta);
+    const frame = meta.presentedFrames;
+    if (frame - latestFrame > 1) {
+      console.log(`Dropped frame ${frame}`);
+    }
+    latestFrame = frame;
+
+    if (frame % 2) {
+      ctx = ctx1;
+      canvas = canvas1;
+    } else {
+      ctx = ctx2;
+      canvas = canvas2;
+    }
 
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    // const bitmap = await createImageBitmap(video);
-
-    // worker.postMessage({ bitmap }, [bitmap]);
-
     const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
     // fs.writeFileSync(
     //   path.resolve(__dirname, `./frames/${frame}.frame`),
     //   pixelData
