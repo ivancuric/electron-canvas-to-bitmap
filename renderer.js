2969bbface95c97cf9d6d0c35ca6c4d2a8e3a0c6
Ivan Čurić <ivan.curic@gmail.com>
2021-07-28 16:53:08 +0200

    camera preview

--- renderer.js
+++ renderer.js
@@ -1,40 +1,19 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const constraints = {
+  audio: false,
+  video: {
+    width: { exact: 3840 },
+    height: { exact: 2160 },
+    frameRate: { exact: 30 },
+  },
+};
 
-const fs = require('fs')
-const path = require('path')
-
-const video = document.getElementById('potato')
-
-video.addEventListener('loadedmetadata', () => {
-  let canvas = new OffscreenCanvas(3840, 2160)
-  let ctx = canvas.getContext('2d')
-  let frame = 0
-
-  const capture = (time, meta) => {
-    console.log(time, meta)
-
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
-
-    queueMicrotask(() => {
-      const s = performance.now()
-      const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data
-      fs.writeFileSync(path.resolve(__dirname, `./frames/${frame}.frame`), pixelData)
-      console.log('asd', performance.now() - s)
-      frame++
-    })
-
-    if (frame > 10) {
-      return
-    }
-
-    video.requestVideoFrameCallback(capture)
+(async () => {
+  try {
+    const video = document.getElementById("video");
+    const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+    const videoTracks = mediaStream.getVideoTracks();
+    video.srcObject = mediaStream;
+  } catch (error) {
+    console.error(error);
   }
-
-  video.play()
-  video.requestVideoFrameCallback(capture)
-})
+})();
